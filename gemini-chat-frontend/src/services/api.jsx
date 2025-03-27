de42c69896dd8c7a3e6c693f56e2bee8a9b96e99
Ajaykumar-058 <ajaykumarbaleboina@gmail.com>
2025-03-27 18:24:45 +0530

    Update api.jsx

--- gemini-chat-frontend/src/services/api.jsx
+++ gemini-chat-frontend/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/api/qna/ask";
+const API_URL = "https://java-app-7mph.onrender.com/api/qna/ask";
 
 export const fetchChatResponse = async (question) => {
     try {
